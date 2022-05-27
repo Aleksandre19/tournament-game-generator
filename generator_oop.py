d03a412a3e3dac474e39b2f685f6f50e9da42944
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2022-05-27 22:46:08 +0200

    Write a function to accept names of the teams

--- generator_oop.py
+++ generator_oop.py
@@ -7,6 +7,27 @@ class GameGenerator:
             if self.teams < 2:
                 print("The minimum number of teams is 2, try again. ")
             else:
+                self.game_team_names(self.teams)
+                break
+
+    def game_team_names(self, teams):
+        self.teams = teams
+        self.team_names = {}
+        while True:
+            name = input(
+                f"Enter the name for team  # {len(self.team_names) + 1}: ")
+
+            if len(name) < 2:
+                print("Team names must have at least 2 characters, try again.")
+            # Chacking to find if name has max 2 characters.
+            elif len(name.split(" ")) > 2:
+                print("Team names may have at most 2 words, try again.")
+            else:
+                self.team_names[name] = 0
+
+            # Breaking the while loop when all names
+            # are edded in to the list
+            if len(self.team_names) >= teams:
                 break
 
 
