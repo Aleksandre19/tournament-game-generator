0c9e1594146d4d17218addbc19c7c8968ad7544f
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2022-05-27 22:48:13 +0200

    Write a function to accept the number of games team has played

--- generator_oop.py
+++ generator_oop.py
@@ -28,6 +28,49 @@ class GameGenerator:
             # Breaking the while loop when all names
             # are edded in to the list
             if len(self.team_names) >= teams:
+                self.number_games_played_by_teams(self.team_names)
+                break
+
+    def number_games_played_by_teams(self, team_names):
+        self.team_names = team_names
+        # Calculating a minimum number of the games for each team
+        min_number = len(self.team_names) - 1
+        count = 0
+        finished = False
+
+        # First loop to get the number of the games
+        while True:
+            games = int(
+                input("Enter the number of games played by each team: "))
+            if games < min_number:
+                print(
+                    "Invalid number of games. Each team plays each other at least once in the regular season, try again.")
+            else:
+                # Gettiing the wins numbers
+                while True:
+                    # Getting the team name from the dictionary
+                    team_name = list(self.team_names.keys())[count]
+
+                    wins = int(
+                        input(f"Enter the number of wins Team {team_name} had: "))
+                    if wins > games:
+                        print(
+                            f"The maximum number of wins is {games}, try again.")
+                    elif wins < 0:
+                        print("The minimum number of wins is 0, try again.")
+                    else:
+                        # After validation adding a wins to the teams accordingly
+                        self.team_names[team_name] = wins
+                        count += 1
+
+                    # When all wins has been added breaking the loop
+                    # and calling the game generator function
+                    if count >= len(self.team_names):
+                        count = 0
+                        finished = True
+                        break
+
+            if finished:
                 break
 
 
